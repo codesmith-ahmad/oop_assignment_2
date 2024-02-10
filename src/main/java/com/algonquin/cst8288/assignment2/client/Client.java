29737a7dfa72c278f3b8297948e070073e73c206
codesmith-ahmad <alias.000@outlook.com>
2024-02-10 12:19:39 -0500

    main method

--- src/main/java/com/algonquin/cst8288/assignment2/client/Client.java
+++ src/main/java/com/algonquin/cst8288/assignment2/client/Client.java
@@ -2,7 +2,32 @@ package com.algonquin.cst8288.assignment2.client;
 
 public class Client {
 	
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+        // Creating Academic Event
+        EventCreator academicEventCreator = new AcademicEventCreator();
+        Event academicEvent = academicEventCreator.createEvent();
+        academicEvent.displayInfo();
+        System.out.println("Admission Fee: $" + academicEvent.calculateAdmissionFee());
+
+        // Creating Public Event
+        EventCreator publicEventCreator = new PublicEventCreator();
+        Event publicEvent = publicEventCreator.createEvent();
+        publicEvent.displayInfo();
+        System.out.println("Admission Fee: $" + publicEvent.calculateAdmissionFee());
+
+        // Database operations
+        DBConnection dbConnection = DBConnection.getInstance();
+        DBOperations dbOperations = new DBOperations();
+        dbOperations.saveEvent(academicEvent);
+        dbOperations.saveEvent(publicEvent);
+
+        // Logging
+        LMSLogger logger = LMSLogger.getInstance();
+        try {
+            // Simulating an exception
+            throw new NullPointerException("Simulated NullPointerException");
+        } catch (NullPointerException e) {
+            logger.log(LogLevel.ERROR, "Exception caught: " + e.getMessage());
+        }
+    }
 }
